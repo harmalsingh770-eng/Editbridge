6fabedd6564d8d6dd9190e97db2cc0938505a3f5
gurnek1911-rgb <gurnek1911@gmail.com>
2026-04-24 21:02:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./lib/**/*.{js,ts,jsx,tsx}"
+],
   theme: {
     extend: {
       fontFamily: {
