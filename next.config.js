2d7938103a2d5690daeedf1183dcc467cb8df747
gurnek1911-rgb <gurnek1911@gmail.com>
2026-04-24 21:00:20 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Ensures all pages render correctly on Vercel
-  // localStorage is only accessed client-side (guarded in db.js)
-}
-
-module.exports = nextConfig
